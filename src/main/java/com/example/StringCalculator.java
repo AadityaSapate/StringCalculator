b3a1de3f1718abb4d09706f17ab914d4dd8628e1
AdityaSapate <adisaps8@gmail.com>
2021-09-08 12:59:24 +0530

    Create a test class StringCalculatorTest following test-first approach

--- src/main/java/com/example/StringCalculator.java
+++ src/main/java/com/example/StringCalculator.java
@@ -2,6 +2,11 @@ package com.example;
 
 public class StringCalculator {
     public static void main(String[] args){
-        
+
+    }
+
+    public int add(String numberString)
+    {
+        return 0;
     }
 }
